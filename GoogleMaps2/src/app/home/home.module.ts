0e4564a77c89242b383c76c229ddc43e0172fd26
BIG CHUNGO <bigchungus@gmail.com>
2019-02-14 18:55:29 -0200

    GuguGaitero

--- GoogleMaps2/src/app/home/home.module.ts
+++ GoogleMaps2/src/app/home/home.module.ts
@@ -1,23 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 
-import { HomePage } from './home.page';
+import {Component, ViewChild, ElementRef} from '@angular/core';
+import {NavController, Platform} from "@ionic/angular";
 
-@NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: HomePage
-      }
-    ])
-  ],
-  declarations: [HomePage]
+@Component({
+  selector:'page-home',
+  templateUrl:'home.page.html'
 })
-export class HomePageModule {}
+export class HomePage{
+  constructor(public navCtrl: NavController){
+
+  }
+}
